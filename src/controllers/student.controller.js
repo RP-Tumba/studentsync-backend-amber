22ab14ac8fbee9e366711c50d09ac6528e756c36
Docile123 <imbereyemasoo@gmail.com>
2025-05-06 18:14:14 +0300

    creating the delete-end-point

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -23,3 +23,39 @@ export const getAllStudents = async (req, res) => {
     });
   }
 };
+export const deleteStudent = async (req, res) => {
+  try {
+              const {id} = req.params;
+            // id checking 
+            if(isNaN(id)){
+              return res.status(404).json({
+                MessageStatus:false,
+                message:`the id ${id} is invalid`
+              })
+            };
+          // ensuring if a student that I want to delele is existed in database'
+            const checkquery = "SELECT * FROM students WHERE id = $1";
+            const checking = await pool.query(checkquery,[id]);
+            if(checking.rows.length===0){
+             return res.status(404).json({
+                MessageStatus:false,
+                message:`the student with this id ${id} not found`});
+            }
+          
+            const deleteqry = "DELETE FROM students WHERE id = $1";
+            await pool.query(deleteqry,[id]);
+            res.status(200).json({
+              MessageStatus:true,
+              message:`Deleting a student is done successfully.`
+            })
+              
+  } catch (err) {
+    
+          logger.error(err.message);
+          res.status(500).json({
+            MessageStatus: false,
+            message: `An unexpected error occurred in DELETE/STUDENT, ${err.message}`,
+          });
+  }
+  
+};
