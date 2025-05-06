078839a4a2873c37720fd34d460eded21351f224
dieuaime-shami <shamidieuaime272@gmail.com>
2025-05-06 23:23:31 +0200

    Update-end-point

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -23,3 +23,33 @@ export const getAllStudents = async (req, res) => {
     });
   }
 };
+
+export const updateStudent= async(req,res)=>{
+  try {
+    const {id}=req.params;
+    const {first_name,last_name,student_id,email,date_of_birth,contact_number,enrollment_date,profile_picture}=req.body;
+      
+     
+    const updateStudent= `UPDATE students SET first_name='${first_name}',last_name ='${last_name}',
+    student_id = '${student_id}',email ='${email}',date_of_birth ='${date_of_birth}',contact_number ='${contact_number}',
+    enrollment_date ='${enrollment_date}',profile_picture ='${profile_picture}' WHERE id= ${id}`
+    const updatedstudent= await pool.query(updateStudent);
+
+    if(updatedstudent.rowCount === 0){
+      return res.status(400).json({
+        success:false,
+        message:"The new student was not recorded"
+      })
+    }
+    res.status(200).json({
+      success:true,
+      message:"the students data was recorded successfully",
+      data:updatedstudent.rows[0]
+    })
+  } catch (err) {
+    res.status(500).json({
+      success:false,
+      message:`An error happened,${err?.message}`
+    })
+  }
+}
